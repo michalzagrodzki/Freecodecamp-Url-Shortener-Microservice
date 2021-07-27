40864986f2598597daee340f3ae65a9641571dce
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-27 19:49:54 +0200

    Refactor url reference

--- controllers/shortener.js
+++ controllers/shortener.js
@@ -22,8 +22,9 @@ exports.get_link = (req, res) => {
 exports.post_link = async (req, res) => {
   try {
     await validatePostRequest(req);
-    const fullLink = getFullLink(req.body.url);
-    const shortLink = await postLink(req.body.url);
+    const { url } = req.body;
+    const fullLink = getFullLink(url);
+    const shortLink = await postLink(url);
     const response = {
       original_url: fullLink,
       short_url: shortLink,
