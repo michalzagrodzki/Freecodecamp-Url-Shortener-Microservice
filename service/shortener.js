a222f381248ad6b888566428d63d59ea6a6974ee
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-27 20:31:40 +0200

    Add methods for handling calls to mongo database

--- service/shortener.js
+++ service/shortener.js
@@ -1,9 +1,32 @@
+const { v4: uuidv4 } = require("uuid");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+mongoose.connect(process.env["DB_URI"], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const shortUrlSchema = new Schema({
+  shortUrlId: { type: String, required: true },
+  fullUrl: { type: String, required: true },
+});
+
 async function get(payload) {
   return "";
 }
 
 async function post(payload) {
-  return "";
+  const guidUrl = uuidv4();
+  const shortUrl = new shortUrlSchema({
+    shortUrlId: guidUrl,
+    fullUrl: payload,
+  });
+  const response = shortUrl.save(function (err, data) {
+    if (err) return done(err);
+    done(null, data);
+  });
+  return response;
 }
 
 exports.get = get;
